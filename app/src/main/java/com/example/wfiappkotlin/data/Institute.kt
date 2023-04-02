9f401296728e51213ae9a1d5876134c2c216a561
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-04-03 00:42:53 +0200

    data model

---
+++ app/src/main/java/com/example/wfiappkotlin/data/Institute.kt
@@ -0,0 +1,7 @@
+package com.example.wfiappkotlin.data
+
+data class Institute(
+    val title: String,
+    val info: String,
+    val imageResource: Int // identyfikatory są przechowywane jako int
+)
